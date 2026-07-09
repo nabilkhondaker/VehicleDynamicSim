76222073b7c19a577fd603b32e8210fcc0adf93a
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-07-08 23:25:55 -0700

    Implement Aerodynamics class with force calculation

---
+++ src/Dynamics/Aerodynamics.cpp
@@ -0,0 +1,18 @@
+#include "ApexSim/Dynamics/Aerodynamics.hpp"
+
+namespace ApexSim::Dynamics {
+
+Aerodynamics::Aerodynamics(AeroMap map) : m_map(map) {}
+
+Core::Vector3 Aerodynamics::calculateForces(Core::scalar_t velocity) const {
+    Core::Vector3 forces = Core::Vector3::Zero();
+    Core::scalar_t dynamicPressure = 0.5 * m_map.airDensity * velocity * velocity * m_map.frontalArea;
+
+    forces[0] = dynamicPressure * m_map.dragCoefficient;        // Drag
+    forces[1] = dynamicPressure * m_map.liftCoefficientFront;   // Front Downforce (Negative lift)
+    forces[2] = dynamicPressure * m_map.liftCoefficientRear;    // Rear Downforce
+
+    return forces;
+}
+
+} // namespace ApexSim::Dynamics
